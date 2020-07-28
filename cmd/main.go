fe25f11175682dce3a15c49973d343edfb1fce2e
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2020-07-28 10:14:51 +0530

    add main file
    Signed-off-by: Ajitem Sahasrabuddhe <ajitem.s@outlook.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
-package cmd
+package main
+
+import (
+	benchApi "bench-api"
+	"log"
+	"net/http"
+)
+
+func main() {
+	server := benchApi.NewServer()
+
+	log.Println("listening to requests on port 8080")
+	err := http.ListenAndServe(":8080", server)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
