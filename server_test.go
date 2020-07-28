90231da42dbe0f58a973ba8dc705566211201d5d
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2020-07-28 10:20:34 +0530

    convert greet to tabular mode
    convert greet test case to tabular mode and add a previously unaccounted for test condition when the greet name is blank
    
    Signed-off-by: Ajitem Sahasrabuddhe <ajitem.s@outlook.com>

--- server_test.go
+++ server_test.go
@@ -43,44 +43,64 @@ func TestGet(t *testing.T) {
 
 func TestGreet(t *testing.T) {
 	// should return {"message": "Hello, <name>!"} where the name is given by the user
-	res, err := DoRequest(testServer, http.MethodGet, "/greet/ajitem")
-	assert.NoError(t, err)
+	var tests = []struct {
+		name string
+		err  bool
+	}{
+		{name: "ajitem"},
+		{name: "radha"},
+		{name: "", err: true},
+	}
 
-	assert.Equal(t, http.StatusOK, res.Code)
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("greet %s", tt.name), func(t *testing.T) {
+			res, err := DoRequest(testServer, http.MethodGet, fmt.Sprintf("/greet/%s", tt.name))
+			assert.NoError(t, err)
 
-	body, err := ioutil.ReadAll(res.Body)
-	assert.NoError(t, err)
-	assert.Equal(t, `{"message": "Hello, ajitem!"}`, string(body))
+			body, err := ioutil.ReadAll(res.Body)
+			assert.NoError(t, err)
+			if tt.err {
+				assert.Equal(t, http.StatusNotFound, res.Code)
+			} else {
+				assert.Equal(t, http.StatusOK, res.Code)
+				assert.Equal(t, fmt.Sprintf(`{"message": "Hello, %s!"}`, tt.name), string(body))
+			}
+		})
+	}
 }
 
 func TestFibonacci(t *testing.T) {
 	// should return {"number": <n>, "fibonacci": <nth fibonacci number>} where the number n
 	// is given by the user.
-	var fibotests = []struct {
+	var tests = []struct {
 		name   string
 		input  string
 		output string
 		err    bool
 	}{
+		{name: "valid input 1", input: "1", output: "1"},
+		{name: "valid input 2", input: "2", output: "1"},
+		{name: "valid input 3", input: "3", output: "2"},
 		{name: "valid input 22", input: "22", output: "17711"},
+		{name: "valid input 32", input: "32", output: "2178309"},
+		{name: "valid input 64", input: "64", output: "10610209857723"},
 		{name: "invalid input string", input: "abcd", err: true},
 	}
 
-	for _, ft := range fibotests {
-		t.Run(ft.name, func(t *testing.T) {
-			res, err := DoRequest(testServer, http.MethodGet, fmt.Sprintf("/fibonacci/%s", ft.input))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := DoRequest(testServer, http.MethodGet, fmt.Sprintf("/fibonacci/%s", tt.input))
 			assert.NoError(t, err)
 
 			body, err := ioutil.ReadAll(res.Body)
 			assert.NoError(t, err)
 
-
-			if ft.err {
+			if tt.err {
 				assert.Equal(t, http.StatusBadRequest, res.Code)
 				assert.Equal(t, `{"error": "fibonacci endpoint accepts only numbers"}`, string(body))
 			} else {
 				assert.Equal(t, http.StatusOK, res.Code)
-				assert.Equal(t, fmt.Sprintf(`{"number": %s, "fibonacci": %s}`, ft.input, ft.output), string(body))
+				assert.Equal(t, fmt.Sprintf(`{"number": %s, "fibonacci": %s}`, tt.input, tt.output), string(body))
 			}
 		})
 	}
