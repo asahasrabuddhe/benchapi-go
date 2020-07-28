e9cdfa7da9220e180702591784e4f41f27bc92f4
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2020-07-28 09:14:59 +0530

    Setup test structure for our application

--- main_test.go
+++ main_test.go
@@ -1,1 +1,26 @@
 package bench_api
+
+import (
+	"os"
+	"testing"
+)
+
+var testServer *server
+
+func TestMain(m *testing.M) {
+	testServer = newServer()
+	os.Exit(m.Run())
+}
+
+func TestGet(t *testing.T) {
+	// should return {"message": "Hello, world!"}
+}
+
+func TestGreet(t *testing.T) {
+	// should return {"message": "Hello, <name>!"} where the name is given by the user
+}
+
+func TestFibonacci(t *testing.T) {
+	// should return {"number": <n>, "fibonacci": <nth fibonacci number>} where the number n
+	// is given by the user.
+}
