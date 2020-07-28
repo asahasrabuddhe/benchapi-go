cc9c0c997f4003faacb89802a272f139391cf79a
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2020-07-28 10:09:36 +0530

    implement fibonacci endpoint
    Signed-off-by: Ajitem Sahasrabuddhe <ajitem.s@outlook.com>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/go-chi/chi"
 	"net/http"
+	"strconv"
 )
 
 type server struct {
@@ -29,6 +30,33 @@ func (s *server) routes() {
 		name := chi.URLParam(request, "name")
 
 		_, _ = fmt.Fprintf(writer, `{"message": "Hello, %s!"}`, name)
+	})
+
+	s.router.Get("/fibonacci/{number}", func(writer http.ResponseWriter, request *http.Request) {
+		numberStr := chi.URLParam(request, "number")
+		number, err := strconv.Atoi(numberStr)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			_, _ = fmt.Fprint(writer, `{"error": "fibonacci endpoint accepts only numbers"}`)
+			return
+		}
+
+		fibonacci := func() func() int64 {
+			a, b := int64(0), int64(1)
+			return func() int64 {
+				defer func() {
+					a, b = b, a+b
+				}()
+				return a
+			}
+		}()
+
+		var result int64
+		for i := 0; i <= number; i++ {
+			result = fibonacci()
+		}
+
+		_, _ = fmt.Fprintf(writer, `{"number": %d, "fibonacci": %d}`, number, result)
 	})
 }
 
