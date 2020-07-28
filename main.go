bc6a167cdf54aecd4ec51f65564206fb4c35940d
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2020-07-28 09:40:03 +0530

    write code for the greet handler
    Signed-off-by: Ajitem Sahasrabuddhe <ajitem.s@outlook.com>

--- main.go
+++ main.go
@@ -24,6 +24,12 @@ func (s *server) routes() {
 	s.router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
 		_, _ = fmt.Fprint(writer, `{"message": "Hello, world!"}`)
 	})
+
+	s.router.Get("/{name}", func(writer http.ResponseWriter, request *http.Request) {
+		name := chi.URLParam(request, "name")
+
+		_, _ = fmt.Fprintf(writer, `{"message": "Hello, %s!"}`, name)
+	})
 }
 
 func (s *server) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
