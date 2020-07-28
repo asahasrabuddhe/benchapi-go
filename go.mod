209663fb312e751f11fc460d8cc83759d9264e14
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2020-07-28 09:33:59 +0530

    implement GET / route
    Signed-off-by: Ajitem Sahasrabuddhe <ajitem.s@outlook.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module bench-api
 
 go 1.14
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/stretchr/testify v1.6.1
+)
--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package bench_api
 
-type server struct {
+import (
+	"fmt"
+	"github.com/go-chi/chi"
+	"net/http"
+)
 
+type server struct {
+	router *chi.Mux
 }
 
 func newServer() *server {
-	return &server{}
+	s := &server{
+		router: chi.NewRouter(),
+	}
+
+	s.routes()
+
+	return s
+}
+
+func (s *server) routes() {
+	s.router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = fmt.Fprint(writer, `{"message": "Hello, world!"}`)
+	})
+}
+
+func (s *server) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	s.router.ServeHTTP(writer, request)
 }
